be8b57486073f7079337ddf0ae705255782ce3a7
JayoonKoo <rnwkdbs12@gmail.com>
2021-12-08 13:49:07 +0900

    feat: create post 구현

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { tweets } from "./mock/tweet";
+import { Tweet, tweets } from "./mock/tweet";
 
 const app = express();
 const port = 8080;
@@ -22,6 +22,21 @@ app.get("/tweets/:id", (req, res) => {
   const { id } = req.params;
   const findById = tweets.find((tweet) => tweet.id === id);
   return res.json(findById);
+});
+
+// create tweet
+app.post("/tweets", (req, res) => {
+  const { text, name, username, url } = req.body;
+  const newTweet: Tweet = {
+    id: String(tweets.length + 1),
+    createdAt: new Date(),
+    name,
+    text,
+    username,
+    url: url,
+  };
+  tweets.push(newTweet);
+  res.json(newTweet);
 });
 
 app.listen(port, () => {
