cd71f84d8fd60adfd4fe1c78d332cc6f465cdf57
JayoonKoo <rnwkdbs12@gmail.com>
2021-12-08 14:48:13 +0900

    feat: router 설정
    tweet router
    추가적으로 404, 500 api 추가

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
-import express from "express";
+import express, { ErrorRequestHandler } from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
+import "express-async-errors";
 import { Tweet, tweets } from "./mock/tweet";
+import tweetsRouter from "./router/tweets";
 
 const app = express();
 const port = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
+app.use(helmet());
+app.use(cors());
+
+// router
+app.use("/tweets", tweetsRouter);
 
 // get Tweets, findByUsername
 app.get("/tweets", (req, res) => {
@@ -57,6 +68,15 @@ app.delete("/tweets/:id", (req, res) => {
   tweets.splice(index, 1);
   res.json(true);
 });
+
+app.use((req, res, next) => {
+  res.sendStatus(404);
+});
+const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
+  console.error(error);
+  res.status(500);
+};
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`server openned ${port}...`);
