ab4757136908159152758412c0c1725a0966bcf1
JayoonKoo <rnwkdbs12@gmail.com>
2021-12-09 14:16:46 +0900

    feat: router 분리

--- src/index.ts
+++ src/index.ts
@@ -18,60 +18,10 @@ app.use(cors());
 // router
 app.use("/tweets", tweetsRouter);
 
-// get Tweets, findByUsername
-app.get("/tweets", (req, res) => {
-  const { username } = req.query;
-  if (!username) {
-    return res.json(tweets);
-  }
-  const findByName = tweets.filter((tweet) => tweet.name === username);
-  return res.json(findByName);
-});
-
-// byid
-app.get("/tweets/:id", (req, res) => {
-  const { id } = req.params;
-  const findById = tweets.find((tweet) => tweet.id === id);
-  return res.json(findById);
-});
-
-// create tweet
-app.post("/tweets", (req, res) => {
-  const { text, name, username, url } = req.body;
-  const newTweet: Tweet = {
-    id: String(Number(tweets[tweets.length - 1].id) + 1),
-    createdAt: new Date(),
-    name,
-    text,
-    username,
-    url: url,
-  };
-  tweets.push(newTweet);
-  res.json(newTweet);
-});
-
-// update tweet
-app.put("/tweets/:id", (req, res) => {
-  const { id } = req.params;
-  const { text } = req.body;
-  let findTweet = tweets.find((tweet) => tweet.id === id);
-  if (findTweet) {
-    findTweet.text = text;
-  }
-  res.json(findTweet);
-});
-
-// Delete tweet
-app.delete("/tweets/:id", (req, res) => {
-  const { id } = req.params;
-  const index = tweets.findIndex((tweet) => tweet.id === id);
-  tweets.splice(index, 1);
-  res.json(true);
-});
-
 app.use((req, res, next) => {
   res.sendStatus(404);
 });
+
 const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
   console.error(error);
   res.status(500);
--- src/router/tweets.ts
+++ src/router/tweets.ts
@@ -1,6 +1,63 @@
 import express from "express";
 import "express-async-errors";
+import { Tweet, tweets } from "../mock/tweet";
 
 const router = express.Router();
+// get Tweets, findByUsername
+router.get("/", (req, res) => {
+  const { username } = req.query;
+  if (!username) {
+    return res.json(tweets);
+  }
+  const findByName = tweets.filter((tweet) => tweet.name === username);
+  return res.json(findByName);
+});
+
+// byid
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const findById = tweets.find((tweet) => tweet.id === id);
+  if (findById) {
+    res.status(200).json(findById);
+  } else {
+    res.status(404).json({ message: `Tweet id(${id}) not found` });
+  }
+});
+
+// create tweet
+router.post("/", (req, res) => {
+  const { text, name, username, url } = req.body;
+  const newTweet: Tweet = {
+    id: Date.now().toString(),
+    createdAt: new Date(),
+    name,
+    text,
+    username,
+    url: url,
+  };
+  tweets.unshift(newTweet);
+  res.status(201).json(newTweet);
+});
+
+// Delete tweet
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const index = tweets.findIndex((tweet) => tweet.id === id);
+  tweets.splice(index, 1);
+  res.sendStatus(204);
+});
+
+// update tweet
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { text } = req.body;
+  let findTweet = tweets.find((tweet) => tweet.id === id);
+  if (findTweet) {
+    findTweet.text = text;
+    res.status(200).json(findTweet);
+  } else {
+    res.status(404).json({ message: `Tweet id(${id}) not found` });
+  }
+});
 
 export default router;
